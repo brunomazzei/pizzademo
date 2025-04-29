01c36e7157f146f442e91b70ac58385fc4c6bdc1
Bruno Mazzei <bmazzei96@gmail.com>
2025-04-29 20:59:24 -0300

    Atualização de comentário

--- src/main/java/com/senac/pizzademo/controller/AutenticacaoController.java
+++ src/main/java/com/senac/pizzademo/controller/AutenticacaoController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/login")
 public class AutenticacaoController {
-    //Teste2 Github comit
+    //Teste2 Github comit 2
     private final AuthenticationManager authenticationManager;
 
     public AutenticacaoController(AuthenticationManager authenticationManager) {
